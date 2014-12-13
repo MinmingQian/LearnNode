46fd6a108ee924ef064c146102880bf66cf56ee0
minmingqian <qianminming@gmail.com>
2014-12-13 11:45:17 +1030

    add index

--- server.js
+++ server.js
@@ -2,16 +2,18 @@
  * Created by minmingqian on 13/12/14.
  */
 
-
 var http = require("http");
 
-function onRequest(request, response) {
-    console.log("Request received");
-    response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write("Hi");
-    response.end();
+function start() {
+    function onRequest(request, response) {
+        console.log("Request received");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hi");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("server has started");
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log("server has started");
+exports.start = start;
